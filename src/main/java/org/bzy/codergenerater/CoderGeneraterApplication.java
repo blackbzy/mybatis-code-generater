64eb514d725d9eabdca74ac622ace8c38f5f5b56
bzy <1131064363@qq.com>
2022-12-14 11:02:51 +0800

    项目初始化

---
+++ src/main/java/org/bzy/codergenerater/CoderGeneraterApplication.java
@@ -0,0 +1,13 @@
+package org.bzy.codergenerater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderGeneraterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderGeneraterApplication.class, args);
+	}
+
+}
